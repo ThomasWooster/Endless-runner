d94fdf5f04bc357704aa3d565cfc495d58e9609a
ThomasWooster <57817076+ThomasWooster@users.noreply.github.com>
2019-12-14 11:44:37 +1100

    11:45 14/12/19 commit

---
+++ New Unity Project/Assets/scripts/PlayerCharacter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    float jumpSpeed = 8.0f;
+    float score;
+    float speed = 5.0f;
+    private float vSpeed = 0.0f; // current vertical velocity
+
+    public float gravity = 12.0f;
+    public Text textref;
+
+    public void incrementScore(int value)
+    {
+        score += value;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity;
+        velocity.x = 0;
+        velocity.y = 0;
+        velocity.z = 0;
+        var controller = GetComponent<CharacterController>();
+
+        if (controller.isGrounded)
+        {
+            vSpeed = 0.0f;  
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            vSpeed = jumpSpeed;
+        }
+
+        vSpeed -= gravity * Time.deltaTime;
+        velocity.y = vSpeed;
+        //print(vSpeed);
+
+        controller.Move(velocity * Time.deltaTime);
+
+
+        textref.text = "Score:" + score;
+    }
+}
---
+++ New Unity Project/Assets/scripts/terainSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terainSpawner : MonoBehaviour
+{
+    float timer;
+    public GameObject prefabTerain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 5)
+        {
+            print("spawn");
+            timer = 0.0f;
+            Instantiate(prefabTerain, transform.position, Quaternion.identity);
+        }
+    }
+}
